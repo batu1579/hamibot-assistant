24a03adde5db653aa9b47059e6aedbdad9331f21
BATU1579 <batu1234567890@outlook.com>
2022-09-16 10:36:51 +0800

    🐞 fix: 修复合并设置时会覆盖旧设置的问题

--- src/lib/projectConfig.ts
+++ src/lib/projectConfig.ts
@@ -4,15 +4,17 @@ interface ProjectConfig {
     name?: string,
     id?: string,
     lastRobot?: RobotInfo,
-    fileMark?: {
-        configFile?: string,
-        scriptFile?: string
-    }
+    fileMark?: FileMarks
 }
 
 interface RobotInfo {
     id?: string,
     name?: string
+}
+
+interface FileMarks {
+    configFile?: string,
+    scriptFile?: string
 }
 
 const CONFIG_FILENAME = 'hamibot.config.json';
@@ -27,21 +29,61 @@ export async function getProjectConfig(): Promise<ProjectConfig> {
 }
 
 /**
- * @description: 更新项目设置文件（不存在会自动创建）。
- * @param {ProjectConfig} config 
+ * @description: 更新项目设置，可以合并更深层次的设置项
+ * @param {ProjectConfig} newConfig 新的设置项。
  */
-export async function updateProjectConfig(config: ProjectConfig): Promise<void> {
-    config = Object.assign(await getProjectConfig(), config);
-    workspace.fs.writeFile(
+export async function updateProjectConfig(newConfig: ProjectConfig): Promise<void> {
+    let config = await getProjectConfig();
+
+    // 合并新旧设置
+    config = mergeObj(config, newConfig);
+    saveProjectConfig(config);
+}
+
+/**
+ * @description: 保存项目设置文件，会覆盖以前的设置（不存在会自动创建）。
+ * @param {ProjectConfig} config 项目设置对象。
+ */
+async function saveProjectConfig(config: ProjectConfig): Promise<void> {
+    await workspace.fs.writeFile(
         getConfigFileUri(),
         Buffer.from(JSON.stringify(config, null, 4))
     );
 }
 
+/**
+ * @description: 获取设置文件的 `Uri` 。
+ * @return {Uri}
+ */
 function getConfigFileUri(): Uri {
     const workspaceFolders = workspace.workspaceFolders;
     if (!workspaceFolders) {
         throw new Error('workspace.workspaceFolders must be provided');
     }
     return Uri.joinPath(workspaceFolders[0].uri, CONFIG_FILENAME);
+}
+
+/**
+ * @description: 合并两个对象，而不是像 `Object.assign()` 只拷贝第一层。
+ * @param {any} target 要合并的目标。
+ * @param {any} origin 要更新的数据。
+ * @return {any} 合并后的 `target` 。
+ */
+// TODO：修改返回值类型与 target 相同
+function mergeObj(target: any, origin: any): any {
+    if (typeof target !== "object" || typeof origin === null) {
+        return target;
+    }
+
+    for (const key in origin) {
+        let copy = origin[key];
+
+        if (typeof copy === "object") {
+            target[key] = mergeObj(target[key], copy);
+        } else {
+            target[key] = copy;
+        }
+    }
+
+    return target;
 }
