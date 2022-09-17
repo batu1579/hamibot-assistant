ac4ee4a3479df3e39d1c482f759fa57e1a6e4f4e
BATU1579 <batu1234567890@outlook.com>
2022-09-17 19:38:55 +0800

    ✨ feat: 添加设置项目名称的指令

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,11 +1,12 @@
-import { ExtensionContext, commands, Uri } from "vscode";
-import { updateProjectConfig } from '../lib/projectConfig';
+import { ExtensionContext, commands } from "vscode";
+import { setProjectName, markConfigFile, markScriptFile } from "./projectConfig";
 
 export function registerCommand(context: ExtensionContext): void {
     context.subscriptions.push(
         // 全局设置
 
         // 项目设置
+        commands.registerCommand("hamibot-assistant.setProjectName", setProjectName),
         commands.registerCommand("hamibot-assistant.markScriptFile", markScriptFile),
         commands.registerCommand("hamibot-assistant.markConfigFile", markConfigFile),
 
--- src/command/projectConfig.ts
+++ src/command/projectConfig.ts
@@ -1,6 +1,16 @@
 import { Uri, window, workspace } from "vscode";
 import { updateProjectConfig } from "../lib/projectConfig";
 
+export async function setProjectName() {
+    let newName = await window.showInputBox({
+        title: "项目名称",
+        prompt: "请输入新的项目名称"
+    });
+    if (newName) {
+        updateProjectConfig({ name: newName });
+    }
+}
+
 export async function markScriptFile(uri: Uri): Promise<void> {
     updateProjectConfig({
         fileMark:
