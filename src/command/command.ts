d18b17acfb03d4ad01a0c7ae25f856501be31ffa
BATU1579 <batu1234567890@outlook.com>
2022-09-18 15:49:16 +0800

    ✨ feat: 添加设置调试机器人的指令

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,5 +1,11 @@
 import { ExtensionContext, commands } from "vscode";
-import { setProjectName, markConfigFile, markScriptFile } from "./projectConfig";
+
+import {
+    setProjectName,
+    markConfigFile,
+    markScriptFile,
+    setExecuteRobot
+} from "./projectConfig";
 
 export function registerCommand(context: ExtensionContext): void {
     context.subscriptions.push(
@@ -9,6 +15,7 @@ export function registerCommand(context: ExtensionContext): void {
         commands.registerCommand("hamibot-assistant.setProjectName", setProjectName),
         commands.registerCommand("hamibot-assistant.markScriptFile", markScriptFile),
         commands.registerCommand("hamibot-assistant.markConfigFile", markConfigFile),
+        commands.registerCommand("hamibot-assistant.setExecuteRobot", setExecuteRobot),
 
         // 操作
     );
--- src/command/projectConfig.ts
+++ src/command/projectConfig.ts
@@ -1,4 +1,6 @@
-import { Uri, window, workspace } from "vscode";
+import { QuickPickItem, Uri, window, workspace } from "vscode";
+
+import { Robot } from "../lib/hamibotApi";
 import { updateProjectConfig } from "../lib/projectConfig";
 
 export async function setProjectName(step: number, totalStep: number): Promise<void> {
@@ -34,4 +36,71 @@ export async function markConfigFile(uri: Uri): Promise<void> {
             configFile: workspace.asRelativePath(uri)
         }
     });
+}
+
+export function setExecuteRobot(step: number, totalStep: number): void {
+    let widget = window.createQuickPick<RobotQuickPickItem | QuickPickItem>();
+    widget.title = "选择调试机器人";
+    widget.matchOnDetail = true;
+    widget.step = step;
+    widget.totalSteps = totalStep;
+    widget.show();
+
+    const refreshItems = async () => {
+        widget.items = [];
+        widget.busy = true;
+        widget.items = [...(await getQuickPickList()), {
+            label: "🔃 刷新",
+            detail: "重新获取机器人列表",
+            alwaysShow: true,
+        }];
+        widget.busy = false;
+    };
+
+    widget.onDidAccept(() => {
+        let item = widget.activeItems[0];
+
+        if (isRobotQuickPickItem(item)) {
+            updateProjectConfig({ lastRobot: item.robotInfo });
+            widget.dispose();
+        } else {
+            refreshItems();
+        }
+    });
+
+    refreshItems();
+}
+
+function isRobotQuickPickItem(item: RobotQuickPickItem | QuickPickItem): item is RobotQuickPickItem {
+    return "robotInfo" in item;
+}
+
+async function getQuickPickList(): Promise<RobotQuickPickItem[]> {
+    let robotList: RobotQuickPickItem[] = [];
+    let showOffline: boolean = workspace.getConfiguration("hamibot-assistant").get("showOfflineRobot")!;
+
+    for (let item of (await Robot.getRobotList()).items) {
+        // 设置为不显示离线时跳过离线的机器人
+        if (!showOffline && !item.online) {
+            continue;
+        }
+
+        robotList.push({
+            label: `${item.online ? "🟢 [在线]" : "🔴 [离线]"} ${item.name}`,
+            detail: `${item.tags.length === 0 ? "" : "[ " + item.tags + " ]"}${item.brand} ${item.model}`,
+            robotInfo: {
+                id: item._id,
+                name: item.name
+            }
+        });
+    }
+
+    return robotList;
+}
+
+interface RobotQuickPickItem extends QuickPickItem {
+    robotInfo: {
+        id: string;
+        name: string;
+    }
 }
