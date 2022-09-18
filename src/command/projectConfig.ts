79ca2241b958749662d13d21309c80f381b9f373
BATU1579 <batu1234567890@outlook.com>
2022-09-18 15:21:39 +0800

    ✨ feat: 修改设置项目名称时传入的参数
    使用 vscode 提供的 createInputBox 来为以后初始化时显示步骤做准备

--- src/command/projectConfig.ts
+++ src/command/projectConfig.ts
@@ -1,14 +1,21 @@
 import { Uri, window, workspace } from "vscode";
 import { updateProjectConfig } from "../lib/projectConfig";
 
-export async function setProjectName() {
-    let newName = await window.showInputBox({
-        title: "项目名称",
-        prompt: "请输入新的项目名称"
+export async function setProjectName(step: number, totalStep: number): Promise<void> {
+    let widget = window.createInputBox();
+    widget.title = "项目名称";
+    widget.prompt = "请输入新的项目名称";
+    widget.step = step;
+    widget.totalSteps = totalStep;
+
+    widget.onDidAccept(() => {
+        if (widget.value) {
+            updateProjectConfig({name: widget.value});
+        }
+        widget.dispose();
     });
-    if (newName) {
-        updateProjectConfig({ name: newName });
-    }
+
+    widget.show();
 }
 
 export async function markScriptFile(uri: Uri): Promise<void> {
