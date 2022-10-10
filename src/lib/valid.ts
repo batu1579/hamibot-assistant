53bf3720563bb5081c952721ca9070f9de43048d
BATU1579 <batu1234567890@outlook.com>
2022-10-10 22:37:43 +0800

    ✨ feat: 添加路径校验函数

--- src/lib/valid.ts
+++ src/lib/valid.ts
@@ -10,6 +10,37 @@ export function isScriptIdValid(id: string): boolean {
     return /^[0-9a-f]{24}$/.test(id);
 }
 
+export function isGithubUrlValid(url: string): boolean {
+    /**
+     * SSH:
+     * 
+     * git@github.com:user/repo
+     * git@github.com:user/repo.git
+     * 
+     * HTTP/HTTPS:
+     * 
+     * http://github.com/user/repo.git
+     * https://github.com/user/repo.git
+     * https://github.com/user/repo
+     */
+    return /^((git@)|(https?:\/\/))github.com[:\/][\w-_]*?\/[\w-_]*?(\.git)?$/.test(url);
+}
+
+export function isLocalPathValid(path: string): boolean {
+    /**
+     * Absolute path
+     * 
+     * D:\GitHub\TypeScript\hamibot-project
+     * D:\GitHub\TypeScript\hamibot-project\
+     * 
+     * Relative path
+     * 
+     * .\template\sampleTemplate
+     * .\template\sampleTemplate\
+     */
+    return /^(([A-Za-z]:)|\.)(.*)$/.test(path);
+}
+
 export function validToken(token?: string): void {
     if (!token) {
         throw new Error('未设置令牌');
@@ -38,4 +69,28 @@ export function validScriptId(scriptId?: string): void {
     if (!isScriptIdValid(scriptId)) {
         throw new Error('脚本 ID 格式错误');
     }
+}
+
+export function validGithubUrl(url?: string): string {
+    if (!url) {
+        throw new Error('未设置 Github 仓库 URL');
+    }
+
+    if (!isGithubUrlValid(url)) {
+        throw new Error('Github 仓库 URL 格式错误');
+    }
+
+    return url;
+}
+
+export function validLocalpath(path?: string): string {
+    if (!path) {
+        throw new Error('未设置本地路径');
+    }
+
+    if (!isLocalPathValid(path)) {
+        throw new Error('本地路径格式错误');
+    }
+
+    return path;
 }
